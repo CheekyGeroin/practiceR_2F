0575ea36da2e26b57243e1102b36d2501d1ba394
Danylo Altukhov <dan.97198@gmail.com>
2023-10-07 22:42:50 +0300

    add components and files

---
+++ src/components/Controlers/Controlers.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+
+export class Controlers extends Component{
+    render() {
+        return
+    }
+}
---
+++ src/components/Statistics/Statictics.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+
+export class Statictics extends Component{
+    render() {
+        return
+    }
+}
